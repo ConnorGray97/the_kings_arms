6e2467d29c8c6736834dc6dd17489ea9e9675415
ConnorGray97 <connor-gray@hotmail.com>
2021-04-21 09:21:40 +0000

    added script for responsive parallax image

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+//Responsive parallax scroll
+
+
+//Resize images on page load
+$(document).ready(function () {
+    resize_all_parallax();
+});
+
+// Resizes images on page resize
+$(window).on('resize', function () {
+    resize_all_parallax();
+});
+
+// Resize function calls for image one.
+function resize_all_parallax() {
+    var div_id = 'img1';
+    var img_w = 1200;
+    var img_h = 636;
+    resize_parallax(div_id, img_w, img_h);
+}
+
+//this resizes the parallax image down to an appropriate size for the viewport 
+function resize_parallax(div_id, img_w, img_h) {
+    var div = $('#' + div_id);
+    var divwidth = div.width();
+    if (divwidth < 769) {
+        var pct = (img_h / img_w) * 105;
+    } else {
+        var pct = 60;
+    }
+    var newheight = Math.round(divwidth * (pct / 100));
+    newheight = newheight + 'px';
+    div.height(newheight);
+}
+// End of responsive parallax scroll
